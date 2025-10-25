e1a2ae6ee6d8936030a88d7729b7a9b8169e874b
tousuienfurukawa-web <tousuien.furukawa@gmail.com>
2025-10-25 16:41:58 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -487,5 +487,4 @@ if __name__ == "__main__":
     import uvicorn
     uvicorn.run(app, host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
 
-# Vercel expects a variable named `app` or `handler`
-handler = app
+
